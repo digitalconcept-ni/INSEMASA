cf3cd582ffb4f02f01af0dad56047738b9af7da5
bryanguevara <bryanguevara38@gmail.com>
2024-06-19 13:47:49 -0600

    SE AGREGARON LAS MODIFICACIONES

---
+++ assets/js/lenguage.js
@@ -0,0 +1,36 @@
+// Obtenemos el contenedor de los elementos
+const btnElements = document.getElementById('language');
+
+// Seleccionamos todas las secciones
+const txtsToChange = document.querySelectorAll('[data-section]');
+
+// Funcion para cargar el archivo segun el idioma
+const changeLanguage = async language => {
+    const requesJson = await fetch(`assets/lenguages/${language}.json`);
+    const texts = await requesJson.json();
+
+    const container = document.getElementById('body');
+    const loader = document.createElement('div');
+    loader.id = 'preloader';
+    container.appendChild(loader);
+
+    setTimeout(function () {
+        for (const txtToChange of txtsToChange) {
+            const section = txtToChange.dataset.section;
+            const value = txtToChange.dataset.value;
+
+            txtToChange.innerHTML = texts[section][value];
+        }
+
+        const preloader = document.querySelector('#preloader');
+        if (preloader) {
+            preloader.remove();
+        }
+    }, 1000)
+
+};
+
+// Evento que captura que btn se selecciona para mostrar el lenguaje
+btnElements.addEventListener('click', (e) => {
+    changeLanguage(e.target.dataset.language);
+});
